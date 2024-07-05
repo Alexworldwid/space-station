a951507351dbeed449179610e900792e6b0951aa
adewale <obadunadewale1234@gmail.com>
2024-07-05 03:21:03 +0100

    tech color

--- src/app/components/shared/technology/navigation.tsx
+++ src/app/components/shared/technology/navigation.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const TechnologyItem: React.FC<Props> = ({ technology, isActive, onClick }) => {
     return (
-        <li key={technology.id} className={`flex items-center justify-center  text-[32px] cursor-pointer w-16 h-16 text-white bg-black border border-white rounded-full hover:bg-white hover:bg-opacity-70 hover:text-black hover:border-black transition-all duration-150 ease-linear ${isActive ? 'bg-white border-black text-black' : ''}`} 
+        <li key={technology.id} className={`flex items-center justify-center  text-[32px] cursor-pointer w-16 h-16 text-white bg-black border border-white rounded-full hover:bg-white hover:bg-opacity-70 hover:text-black hover:border-black transition-all duration-150 ease-linear ${isActive ? 'bg-white border-black text-[#000000]' : ''}`} 
         onClick={onClick}
         >
             <p>{technology.id}</p>
